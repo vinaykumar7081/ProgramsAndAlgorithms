f6491a58dac99750c6d804e27b685b1e9eb03c41
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-05-10 18:13:12 +0530

    [VINAY] Add: Performing the Operation In Bank Debiting balance and Crediting balanced

--- GenericsProblemAndAlgorithm/Program.cs
+++ GenericsProblemAndAlgorithm/Program.cs
@@ -4,19 +4,22 @@ public class Program
 
 {
     const string STACKFILE_PATH = @"D:\AlgorithmProblems\ProgramsAndAlgorithms\GenericsProblemAndAlgorithm\Utility\stackElements'.txt";
+    const string QUEUEFILE_PATH = @"D:\AlgorithmProblems\ProgramsAndAlgorithms\GenericsProblemAndAlgorithm\Utility\Persons.txt";
     public static void Main(String[] args)
     {
         Console.WriteLine(" ####### WELCOME IN THE PROGRAMS AND ALGORITHMS ####### ");
         LinkedList<string> linkedLists = new LinkedList<string>();
         StackOpration<string> stack = new StackOpration<string>();
+        GenericsProblemAndAlgorithm.Queue<string> queue = new GenericsProblemAndAlgorithm.Queue<string>();
         bool check = true;
         while (check)
         {
-            Console.WriteLine(" Adding Elements in Stack 1\n Displaying Stack Elements 2\n Checking for the Parenthesys 3\n ComparingParenthesysy 4\n");
+            Console.WriteLine(" Adding Elements in Stack 1\n Displaying Stack Elements 2\n Checking for the Parenthesys 3\n ComparingParenthesysy 4\n Displaying Queue Elements 5\n Debited Amount 6\n Credite Amount in Account 7\n");
         
             Console.WriteLine("Enter the Above Optionb to the Excute the program");
             int option = Convert.ToInt32(Console.ReadLine());
 
+            
             switch (option)
             {
                 case 1:
@@ -36,6 +39,26 @@ public class Program
 
                 case 4:
                     stack.ComparingParenthesys();
+                    break;
+                case 5:
+                    string person_Queue = File.ReadAllText(QUEUEFILE_PATH);
+                    string[] person_Array = person_Queue.Split(',');
+                    foreach (string s in person_Array)
+                    { 
+                    queue.Enqueue(s);
+                    }
+                    break;
+                case 6:
+                    decimal total_Cash = 500000;
+                    decimal minAmount = 1000;
+                    decimal maxAmount = 35000;
+                    queue.DepositeCashCounter(total_Cash, minAmount, maxAmount);
+                    break;
+                case 7:
+                    decimal total_Cashes = 500000;
+                    decimal credite_minAmount = 1000;
+                    decimal credite_maxAmount = 50000;
+                    queue.CrediteCashCounter(total_Cashes, credite_minAmount, credite_maxAmount);
                     break;
                 case 0:
                     check = false;
---
+++ GenericsProblemAndAlgorithm/Queue.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsProblemAndAlgorithm
+{
+    public class Queue<T> where T : IComparable
+
+    {
+        private Node<T> top;
+        Node<T> head = null;
+        decimal total_Amount;
+        public Queue()
+        {
+            this.top = null;
+            this.total_Amount = 0;
+        }
+        public void Enqueue(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node<T> temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+                Console.WriteLine("inserted into Queue" + node.data);
+            }
+            DisplayQueue();
+        }
+        public void DisplayQueue()
+        {
+            Node<T> temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+            Console.WriteLine(" ");
+        }
+        internal void Dequeue()
+        {
+            if (this.head == null)
+            {
+                Console.WriteLine("Queue is empty, Deletion is not possible");
+                return;
+            }
+            else
+            {
+                while (this.head != null)
+                {
+                    Console.WriteLine("Value Dequeued is " + this.head.data);
+                    this.head = this.head.next;
+                }
+            }
+        }
+        public void DepositeCashCounter(decimal total_cash, decimal minAmount,decimal maxAmount)
+        {
+          
+            while (this.head != null)
+            {
+                Console.WriteLine("Enter the Deposite Amount");
+                decimal depositAmount=Convert.ToDecimal(Console.ReadLine());
+                if (minAmount < depositAmount && depositAmount < maxAmount)
+                {
+                    total_cash = total_cash - depositAmount;
+                    Console.WriteLine("Total  Amount is :" + total_cash);
+                }
+                else 
+                {
+                Console.WriteLine(" $$$$$$$$$ Please Enter the Valid Amount ####### ");
+                }
+                this.head = this.head.next;
+            }
+            Console.WriteLine("Total  Amount is :" + total_cash);
+        }
+        public void CrediteCashCounter(decimal total_cash, decimal minAmount, decimal maxAmount)
+        {
+            while (this.head != null)
+            {
+                Console.WriteLine("Enter the Credite Amount");
+                decimal crediteAmount = Convert.ToDecimal(Console.ReadLine());
+                if (minAmount < crediteAmount && crediteAmount < maxAmount)
+                {
+                    total_cash = total_cash + crediteAmount;
+                    Console.WriteLine("Total  Amount is :" + total_cash);
+                }
+                else
+                {
+                    Console.WriteLine(" $$$$$$$$$ Please Enter the Valid Amount ####### ");
+                }
+                this.head = this.head.next;
+            }
+            Console.WriteLine("Total  Amount is :" + total_cash);
+        }
+    }
+}
