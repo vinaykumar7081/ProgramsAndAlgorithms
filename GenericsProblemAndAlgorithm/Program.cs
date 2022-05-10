d5ea80176bde3b20622633d65c03cbdd61744e0d
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-05-10 16:00:02 +0530

    [VINAY] Add: checking the parenthesys is balanced in the Arithmetic Expression

--- GenericsProblemAndAlgorithm/Program.cs
+++ GenericsProblemAndAlgorithm/Program.cs
@@ -3,21 +3,16 @@ using GenericsProblemAndAlgorithm;
 public class Program
 
 {
-    const string FILE_PATH = @"D:\AlgorithmProblems\ProgramsAndAlgorithms\GenericsProblemAndAlgorithm\Utility\Contents.txt";
-
-    const string INTEGERFILE_PATH = @"D:\AlgorithmProblems\ProgramsAndAlgorithms\GenericsProblemAndAlgorithm\Utility\Integer.txt";
-    const string NUMBERFILE_PATH = @"D:\AlgorithmProblems\ProgramsAndAlgorithms\GenericsProblemAndAlgorithm\Utility\integer.txt";
+    const string STACKFILE_PATH = @"D:\AlgorithmProblems\ProgramsAndAlgorithms\GenericsProblemAndAlgorithm\Utility\stackElements'.txt";
     public static void Main(String[] args)
     {
         Console.WriteLine(" ####### WELCOME IN THE PROGRAMS AND ALGORITHMS ####### ");
-        GenericsProblemAndAlgorithm.LinkedListOperation<string> operation = new GenericsProblemAndAlgorithm.LinkedListOperation<string>();
-        LinkedLists<string> linkedLists = new LinkedLists<string>();
-        LinkedList<string> lists = new LinkedList<string>();
-        LinkedList<string> int_list = new LinkedList<string>();
+        LinkedList<string> linkedLists = new LinkedList<string>();
+        StackOpration<string> stack = new StackOpration<string>();
         bool check = true;
         while (check)
         {
-            Console.WriteLine("Display List Elements 1\n  Additino elements in the list2\n Removing the present Elements in  the List 3\n Adding new elements in  the list 4\n Adding integer elements in the list 5\n Sorting The List Elements 6\n Display Size of the List 7\n Apped new Elements8\n Deleting the Matching Elements 9\n");
+            Console.WriteLine(" Adding Elements in Stack 1\n Displaying Stack Elements 2\n Checking for the Parenthesys 3\n ComparingParenthesysy 4\n");
         
             Console.WriteLine("Enter the Above Optionb to the Excute the program");
             int option = Convert.ToInt32(Console.ReadLine());
@@ -25,74 +20,22 @@ public class Program
             switch (option)
             {
                 case 1:
-                    string input_String = File.ReadAllText(FILE_PATH);
-                    string[] array = input_String.Split(',');
-                    foreach (string s in array)
-                    {
-                        lists.AddLast(s);
+                    string expression = File.ReadAllText(STACKFILE_PATH);
+                    string[] Expression_Array = expression.Split(',');
+                    foreach (string s in Expression_Array)
+                    { 
+                    stack.Push(s);
                     }
-                    operation.AddingElements(lists);
                     break;
                 case 2:
-                    operation.Display();
+                    stack.Display();
                     break;
                 case 3:
-                    Console.WriteLine("Enter the Word to Search in the List");
-                    string input_string = Console.ReadLine();
-                    operation.Check_Removing_PresentElements(input_string);
-                        break;
-                case 4:
-                    Console.WriteLine("Enter the Word to Search in the List");
-                    string input_strings = Console.ReadLine();
-                    operation.Check_Added_NewElement(input_strings);
+                    stack.BalancedParenthesys();
                     break;
-                case 5:
-                    string integer_String = File.ReadAllText(INTEGERFILE_PATH);
-                    string[] integer_Array = integer_String.Split(',');
-                    SortedList<string> input = new SortedList<string>();
-                    foreach (string s in integer_Array)
-                    {
-                        int_list.AddLast(s);
-                    }
-                    input.AddingIntegerElements(int_list);
-                    input.Display();
-                    string integer_Strings = File.ReadAllText(NUMBERFILE_PATH);
-                    string[] integer_Arrays = integer_String.Split(',');
-                   
-                    foreach (string s in integer_Array)
-                    {
-                        linkedLists.AddNode(s);
-                    }
-                    linkedLists.DisplaySortedList();
-                    break;
-                case 6:
-                    linkedLists.SortOrderList();
-                    break;
-                case 8:
-                    Console.WriteLine("Enter the Number to Search in the List");
-                    string inputs = Console.ReadLine();
-                    int node = linkedLists.Search(inputs);
-                    
-                        if (node == 0)
-                        {
-                            linkedLists.Append("input");
-                        }
-                    
-                    break;
-                case 7:
-                   int length=linkedLists.Size();
-                    Console.WriteLine("Size of the List is:->"+length);
-                    break;
-                case 9:
-                    Console.WriteLine("Enter the Number to Search in the List");
-                    string input_Data = Console.ReadLine();
-                    int search = linkedLists.Search(input_Data);
-                    linkedLists.Size();
-                    linkedLists.DisplaySortedList();
-                    linkedLists.DeleteNodeAtPerticularPosition(search);
-                    linkedLists.Size();
-                    linkedLists.DisplaySortedList();
 
+                case 4:
+                    stack.ComparingParenthesys();
                     break;
                 case 0:
                     check = false;
---
+++ GenericsProblemAndAlgorithm/StackOpration.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsProblemAndAlgorithm
+{
+    public class StackOpration<T>
+    {
+        private Node<T> top;
+        Node<T> head = null;
+        public StackOpration()
+        {
+            this.top = null;
+        }
+        public void Push(T value)
+        {
+            Node<T> node = new Node<T>(value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack" + value);
+        }
+        internal void Peek()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+            Console.WriteLine("{0} is in the top of the stack", this.top.data);
+            Display();
+        }
+        public void Pop()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty, Deletion is not possible");
+                return;
+            }
+            Console.WriteLine("Value popped is " + this.top.data);
+            top = top.next;
+            //.Display();
+        }
+        internal void IsEmpty()
+        {
+            while (this.top != null)
+            {
+                Peek();
+                Pop();
+            }
+        }
+        public void Display()
+        {
+            Node<T> temp = this.top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+            Console.WriteLine(" ");
+        }
+        int openParenthesiscount = 0;
+        int closeParenthesisCount = 0;
+        int number = 0;
+        public void BalancedParenthesys()
+        { 
+        Node<T> temp = this.top;
+            while(temp != null)
+            {
+                if (temp.data.Equals( ")" ))
+                {
+                    closeParenthesisCount++;
+                }
+                if (temp.data.Equals( "(" ))
+                {
+                    openParenthesiscount++;
+                }
+                else
+                {
+                    number++;
+                }
+                temp = temp.next;
+            }
+        }
+        public void ComparingParenthesys()
+        {
+            if (openParenthesiscount == closeParenthesisCount)
+            {
+                Console.WriteLine("Parenthesys is Balanced");
+            }
+            else 
+            {
+                Console.WriteLine("Parenthesys is not Balanced");
+            }
+        }
+    }
+}
